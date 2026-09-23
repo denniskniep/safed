826b86b1ce5b6f883b87a734d163dfe460783d08
Michael Staehler <michael.staehler@opitz-consulting.com>
2026-09-23 09:24:45 +0200

    bugfix safe parsing url to not propagate errors

---
+++ src/test/java/de/denniskniep/safed/common/UrlUtilsTest.java
@@ -0,0 +1,15 @@
+package de.denniskniep.safed.common;
+
+import de.denniskniep.safed.common.utils.UrlUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class UrlUtilsTest {
+
+    @Test
+    void sanitize_doesNotThrow_onIllegalQueryCharacters() {
+        var url = "https://fonts.googleapis.com/css?family=Noto+Serif|Noto+Sans";
+        assertDoesNotThrow(() -> UrlUtils.laxStartsWith(url, "https://idp.example.com/auth"));
+    }
+}
